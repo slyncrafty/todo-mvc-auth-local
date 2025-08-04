b15dc198c94817291b462707b52ca4dc84d00a10
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-08-05 08:39:18 +0900

    Updated Mongoose queries to async/await per Mongoose 6+ compatibility.

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,7 +56,7 @@ const User = require('../models/User')
     })
   }
   
-  exports.postSignup = (req, res, next) => {
+  exports.postSignup = async (req, res, next) => {
     const validationErrors = []
     if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
     if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
@@ -74,23 +74,25 @@ const User = require('../models/User')
       password: req.body.password
     })
   
-    User.findOne({$or: [
-      {email: req.body.email},
-      {userName: req.body.userName}
-    ]}, (err, existingUser) => {
-      if (err) { return next(err) }
+    try{
+      const existingUser = await User.findOne({
+        $or: [
+          { email: req.body.email },
+          { userName: req.body.userName},
+        ]
+      });
       if (existingUser) {
-        req.flash('errors', { msg: 'Account with that email address or username already exists.' })
+         req.flash('errors', { msg: 'Account with that email address or username already exists.' })
         return res.redirect('../signup')
       }
-      user.save((err) => {
-        if (err) { return next(err) }
-        req.logIn(user, (err) => {
+      await user.save();
+      req.logIn(user, (err) => {
           if (err) {
             return next(err)
           }
           res.redirect('/todos')
         })
-      })
-    })
+      } catch (err) {
+    return next(err);
+    }
   }
